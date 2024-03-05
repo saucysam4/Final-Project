e684bd1907852a77038c264a2b96f90abf9136ee
saucysam4 <levinsam2@gmail.com>
2024-03-05 10:39:57 +0200

    Base

---
+++ script.js
@@ -0,0 +1,22 @@
+var currentCardIndex = 0;
+var flashcards = document.getElementsByClassName('flashcard');
+
+function flipCard(index) {
+  var card = flashcards[index - 1];
+  if (card.style.transform === 'rotateY(180deg)') {
+    card.style.transform = 'rotateY(0deg)';
+  } else {
+    card.style.transform = 'rotateY(180deg)';
+  }
+}
+
+function nextCard() {
+  if (currentCardIndex < flashcards.length - 1) {
+    // Hide the current card
+    flashcards[currentCardIndex].style.display = 'none';
+    
+    // Show the next card
+    currentCardIndex++;
+    flashcards[currentCardIndex].style.display = 'block';
+  }
+}
