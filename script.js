5e7ec83e320339245b40eacae2550ca5594b844f
saucysam4 <levinsam2@gmail.com>
2024-03-10 02:47:08 +0200

    Final

--- script.js
+++ script.js
@@ -1,32 +1,137 @@
-var currentCardIndex = 0;
-var flashcards = document.getElementsByClassName('flashcard');
+const container = document.querySelector
+(".container");
+const addQuestionCard = document.getElementById
+("add-question-card");
+const cardButton = document.getElementById
+("save-btn");
+const question = document.getElementById
+("question");
+const answer = document.getElementById
+("answer");
+const errorMessage = document.getElementById
+("error");
+const addQuestion = document.getElementById
+("add-flashcard");
+const closeBtn = document.getElementById
+("close-btn");
+let editBool = false;
 
-function flipCard(index) {
-  var card = flashcards[index - 1];
-  if (card.style.transform === 'rotateY(180deg)') {
-    card.style.transform = 'rotateY(0deg)';
-  } else {
-    card.style.transform = 'rotateY(180deg)';
-  }
+// Add question when user clicks 'Add Flashcard' button
+addQuestion.addEventListener("click", () => {
+    container.classList.add("hide");
+    question.value = "";
+    answer.value = "";
+    addQuestionCard.classList.remove("hide");
+});
+
+// Hide Create Flashcard Card
+closeBtn.addEventListener("click", 
+(hideQuestion = () => {
+    container.classList.remove("hide");
+    addQuestionCard.classList.add("hide");
+    if(editBool) {editBool = false;
+        submitQuestion();
+     }
+    })
+);
+
+// Submit Question
+cardButton.addEventListener("click", 
+(submitQuestion = () => {
+    editBool = false;
+    tempQuestion = question.value.trim();
+    tempAnswer = answer.value.trim();
+    if(!tempQuestion || !tempAnswer) {
+        errorMessage.classList.remove("hide");
+    }
+    else {
+        container.classList.remove("hide");
+        errorMessage.classList.add("hide");
+        viewlist();
+        question.value = "";
+        answer.value = "";
+    }
+    })
+);
+
+// Card generate
+function viewlist() {
+    var listCard = document.getElementsByClassName
+    ("card-list-container");
+    var div = document.createElement("div");
+    div.classList.add("card");
+    // Question
+    div.innerHTML += `
+    <p class="question-div">${question.value}</p>`;
+    // Answer
+    var displayAnswer = document.createElement("p");
+    displayAnswer.classList.add("answer-div", "hide");
+    displayAnswer.innerText = answer.value;
+    
+    // Link to show/hide answer
+    var link =document.createElement("a");
+    link.setAttribute("href", "#");
+    link.setAttribute("class","show-hide-btn");
+    link.innerHTML = "Show/Hide";
+    link.addEventListener("click", ()=> 
+    {displayAnswer.classList.toggle("hide");
+    link.addEventListener("click", (event) => {
+        event.preventDefault(); // Prevent the default behavior of the anchor element
+        displayAnswer.classList.toggle("hide");
+    });
+});
+ 
+    div.appendChild(link);
+    div.appendChild(displayAnswer);
+
+
+    // Edit button
+    let buttonsCon = document.createElement("div");
+    buttonsCon.classList.add("buttons-con");
+    var editButton = document.createElement("button");
+    editButton.setAttribute("class", "edit");
+    editButton.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
+    editButton.addEventListener("click", ()=> {
+        editBool = true;
+        modifyElement(editButton, true);
+        addQuestionCard.classList.remove("hide");
+    });
+    buttonsCon.appendChild(editButton);
+    disableButtons(false);
+
+
+    // Delete Button
+    var deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "delete");
+    deleteButton.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+    deleteButton.addEventListener("click", () => {
+        modifyElement(deleteButton);
+    });
+    buttonsCon.appendChild(deleteButton);
+    
+    
+    div.appendChild(buttonsCon);
+    listCard[0].appendChild(div);
+    hideQuestion();
 }
 
-function nextCard() {
-  if (currentCardIndex < flashcards.length - 1) {
-    // Hide the current card
-    flashcards[currentCardIndex].style.display = 'none';
-    
-    // Show the next card
-    currentCardIndex++;
-    flashcards[currentCardIndex].style.display = 'block';
-  }
-}
-function prevCard() {
-  if (currentCardIndex > 0) {
-    // Hide the current card
-    flashcards[currentCardIndex].style.display = 'none';
-    
-    // Show the previous card
-    currentCardIndex--;
-    flashcards[currentCardIndex].style.display = 'block';
-  }
-}
+// Modify Elements
+const modifyElement = (element,edit=false) => {
+    let parentDiv = element.parentElement.parentElement;
+    let parentQuestion = parentDiv.querySelector(".question-div").innerText;
+    if(edit) {
+        let parentAns = parentDiv.querySelector(".answer-div").innerText;
+        answer.value = parentAns;
+        question.value = parentQuestion;
+        disableButtons(true);
+    }
+    parentDiv.remove();
+};
+
+// Disable edit and delete buttons
+const disableButtons = (value) => {
+    let editButtons = document.getElementsByClassName("edit");
+    Array.from(editButtons).forEach(element => {
+        element.disabled = value;
+    });
+};
